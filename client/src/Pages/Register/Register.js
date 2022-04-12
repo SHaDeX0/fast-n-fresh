65979d3ea897ffa25012a1b3df0698f1ee8b4d5f
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-04-12 22:16:37 +0530

    todo

--- client/src/Pages/Register/Register.js
+++ client/src/Pages/Register/Register.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Register.css';
-import Axios from 'axios';
 
 const Register = () => {
 	const [username, setUsername] = React.useState('');
@@ -26,85 +25,83 @@ const Register = () => {
 		z.style.left = '0px';
 	}
 	return (
-		
-			<div className='SignInPage'>
-				<div className='form-box'>
-					<div className='button-box'>
-						<div id='bttn'></div>
-						<button
-							type='button'
-							className='toggle-btn'
-							onClick={login}
-						>
-							Log In
-						</button>
-						<button
-							type='button'
-							className='toggle-btn'
-							onClick={signup}
-						>
-							Sign Up
-						</button>
-					</div>
-					<form id='login' className='input-group'>
-						<input
-							type='email'
-							className='input-field'
-							placeholder='E-mail Id'
-							required
-							onChange={handleChange}
-						/>
-						<input
-							type='text'
-							className='input-field'
-							placeholder='Enter Password'
-							required
-						/>
-						<button type='Submit' className='submit-btn'>
-							Log In
-						</button>
-					</form>
-
-					<form id='signup' className='input-group'>
-						<input
-							type='text'
-							className='input-field'
-							placeholder='Name'
-							required
-						/>
-						<input
-							type='number'
-							className='input-field'
-							placeholder='Phone Number'
-							required
-						/>
-						<input
-							type='email'
-							className='input-field'
-							placeholder='Email Id'
-							required
-						/>
-						<input
-							type='text'
-							className='input-field'
-							placeholder='Enter Password'
-							required
-						/>
-						<input
-							type='text'
-							className='input-field'
-							placeholder='Re-Enter Password'
-							required
-						/>
-						<input type='checkbox' className='check-box' />
-						<span>I agree to the Terms and Conditions</span>
-						<button type='Submit' className='submit-btn'>
-							Sign Up
-						</button>
-					</form>
+		<div className='SignInPage'>
+			<div className='form-box'>
+				<div className='button-box'>
+					<div id='bttn'></div>
+					<button
+						type='button'
+						className='toggle-btn'
+						onClick={login}
+					>
+						Log In
+					</button>
+					<button
+						type='button'
+						className='toggle-btn'
+						onClick={signup}
+					>
+						Sign Up
+					</button>
 				</div>
+				<form id='login' className='input-group'>
+					<input
+						type='email'
+						className='input-field'
+						placeholder='E-mail Id'
+						required
+						onChange={handleChange}
+					/>
+					<input
+						type='text'
+						className='input-field'
+						placeholder='Enter Password'
+						required
+					/>
+					<button type='Submit' className='submit-btn'>
+						Log In
+					</button>
+				</form>
+
+				<form id='signup' className='input-group'>
+					<input
+						type='text'
+						className='input-field'
+						placeholder='Name'
+						required
+					/>
+					<input
+						type='number'
+						className='input-field'
+						placeholder='Phone Number'
+						required
+					/>
+					<input
+						type='email'
+						className='input-field'
+						placeholder='Email Id'
+						required
+					/>
+					<input
+						type='text'
+						className='input-field'
+						placeholder='Enter Password'
+						required
+					/>
+					<input
+						type='text'
+						className='input-field'
+						placeholder='Re-Enter Password'
+						required
+					/>
+					<input type='checkbox' className='check-box' />
+					<span>I agree to the Terms and Conditions</span>
+					<button type='Submit' className='submit-btn'>
+						Sign Up
+					</button>
+				</form>
 			</div>
-		
+		</div>
 	);
 };
 
