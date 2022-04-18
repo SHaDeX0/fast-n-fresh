e3a1ee14dfdd061b3f3caaf7f17063154c91b7a8
nprateekapai <nprateekapai@gmail.com>
2022-04-19 02:11:29 +0530

    commented my card code

--- client/src/Pages/Menu/Menu.js
+++ client/src/Pages/Menu/Menu.js
@@ -1,62 +1,76 @@
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Button, CardActions, CardContent, CardMedia, Typography, CardActionArea, Card, Grid } from '@mui/material'
+// import { Button, CardActions, CardContent, CardMedia, Typography, CardActionArea, Card, Grid } from '@mui/material'
 
 const Menu = () => {
-	// const [foodItems, setFoodItems] = useState([])
+	const [foodItems, setFoodItems] = useState([])
 
-	// const getFoodItems = async () => {
-	// 	await Axios.get('http://localhost:5000/menu')
-	// 		.then(res => {
-	// 			setFoodItems(res.data)
-	// 		})
-	// 		.catch(err => console.log(err))
-	// }
+	const getFoodItems = async () => {
+		await Axios.get('http://localhost:5000/menu')
+			.then(res => {
+				setFoodItems(res.data)
+			})
+			.catch(err => console.log(err))
+	}
 
-	// useEffect(() => {
-	// 	getFoodItems()
-	// }, [])
+	useEffect(() => {
+		getFoodItems()
+	}, [])
 
-	// return (
-	// 	<>
-	// 		{foodItems.map(item => (
-	// 			<ol key={item.id}>
-	// 				<li>Name : {item.name}</li>
-	// 				{item.description ? (
-	// 					<li>Description : {item.description}</li>
-	// 				) : (
-	// 					<></>
-	// 				)}
-	// 				<li>Price : {item.price}</li>
-	// 				<br />
-	// 			</ol>
-	// 		))}
-	// 	</>
-	// )
 	return (
-		<div>
-			<Grid container spacing={3}>
-				<Grid item xs={3}>
-					<Card sx={{ maxWidth: 345 }}>
-						<CardActionArea>
-							<CardMedia component='img' height='140' image='/static/images/cards/contemplative-reptile.jpg' alt='green iguana' />
-							<CardContent>
-								<Typography gutterBottom variant='h5' component='div'>
-									Diet Coke
-								</Typography>
-							</CardContent>
-						</CardActionArea>
-						<CardActions>
-							<Typography>Todo : increment and decrement quantity </Typography>
-							<Button variant='outlined' size='small' color='primary'>
-								Add to cart
-							</Button>
-						</CardActions>
-					</Card>
-				</Grid>
-			</Grid>
-		</div>
-	)
-}
+		
+		// <div className="wrapper">
+		// <Carditems img=" " title="Diet Coke" />
+		// <Carditems />
+		// <Carditems />
+		// </div>
+
+		<>
+			{foodItems.map(item => (
+				<ol key={item.id}>
+					<li>Name : {item.name}</li>
+					{item.description ? (
+						<li>Description : {item.description}</li>
+					) : (
+						<> </>
+					)}
+					<li>Price : {item.price}</li>
+					<br />
+				</ol>
+			))}
+		</>
+	 )
+
+
+	//prat code 
+
+
+// 	function Carditems(props){
+// 	return (
+// 		<div>
+// 			<Grid container spacing={3}>
+// 				<Grid item xs={3}>
+// 					<Card sx={{ maxWidth: 345 }}>
+// 						<CardActionArea>
+// 							<CardMedia component='img' height='140' image={props.img}  />
+// 							<CardContent>
+// 								<Typography gutterBottom variant='h5' component='div'>
+// 									{props.title}
+// 								</Typography>
+// 							</CardContent>
+// 						</CardActionArea>
+// 						<CardActions>
+// 							<Typography>Todo : increment and decrement quantity </Typography>
+// 							<Button variant='outlined' size='small' color='primary'>
+// 								Add to cart
+// 							</Button>
+// 						</CardActions>
+// 					</Card>
+// 				</Grid>
+// 			</Grid>
+// 		</div>
+// 	)
+// 	}
+ }
 
 export default Menu
