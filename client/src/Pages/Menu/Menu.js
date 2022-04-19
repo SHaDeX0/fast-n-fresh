bc51628c785afc55fc4209812e55d56752c98f1a
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-04-19 17:45:12 +0530

    small changes in Menu and CustomCard

--- client/src/Pages/Menu/Menu.js
+++ client/src/Pages/Menu/Menu.js
@@ -1,6 +1,5 @@
 import Axios from 'axios'
 import React, { useEffect, useState } from 'react'
-// import { Button, CardActions, CardContent, CardMedia, Typography, CardActionArea, Card, Grid } from '@mui/material'
 import CustomCard from './CustomCard'
 
 const Menu = () => {
@@ -28,30 +27,6 @@ const Menu = () => {
 			))}
 		</>
 	)
-	// return (
-	// 	<div>
-	// 		<Grid container spacing={3}>
-	// 			<Grid item xs={3}>
-	// 				<Card sx={{ maxWidth: 345 }}>
-	// 					<CardActionArea>
-	// 						<CardMedia component='img' height='140' image='/static/images/cards/contemplative-reptile.jpg' alt='green iguana' />
-	// 						<CardContent>
-	// 							<Typography gutterBottom variant='h5' component='div'>
-	// 								Diet Coke
-	// 							</Typography>
-	// 						</CardContent>
-	// 					</CardActionArea>
-	// 					<CardActions>
-	// 						<Typography>Todo : increment and decrement quantity </Typography>
-	// 						<Button variant='outlined' size='small' color='primary'>
-	// 							Add to cart
-	// 						</Button>
-	// 					</CardActions>
-	// 				</Card>
-	// 			</Grid>
-	// 		</Grid>
-	// 	</div>
-	// )
 }
 
 export default Menu
