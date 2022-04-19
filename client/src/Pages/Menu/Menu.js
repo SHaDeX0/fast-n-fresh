72a8a68f10b553fd86c796e15db064cfe65fc00d
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-04-19 08:45:47 +0530

    ...

--- client/src/Pages/Menu/Menu.js
+++ client/src/Pages/Menu/Menu.js
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 
 const Menu = () => {
 	const [foodItems, setFoodItems] = useState([])
+	// const [foodItems, setFoodItems] = useState([])
 
 	const getFoodItems = async () => {
 		await Axios.get('http://localhost:5000/menu')
@@ -12,33 +13,60 @@ const Menu = () => {
 			})
 			.catch(err => console.log(err))
 	}
+	// const getFoodItems = async () => {
+	// 	await Axios.get('http://localhost:5000/menu')
+	// 		.then(res => {
+	// 			setFoodItems(res.data)
+	// 		})
+	// 		.catch(err => console.log(err))
+	// }
 
 	useEffect(() => {
 		getFoodItems()
 	}, [])
+	// useEffect(() => {
+	// 	getFoodItems()
+	// }, [])
 
+	// return (
+	// 	<>
+	// 		{foodItems.map(item => (
+	// 			<ol key={item.id}>
+	// 				<li>Name : {item.name}</li>
+	// 				{item.description ? (
+	// 					<li>Description : {item.description}</li>
+	// 				) : (
+	// 					<></>
+	// 				)}
+	// 				<li>Price : {item.price}</li>
+	// 				<br />
+	// 			</ol>
+	// 		))}
+	// 	</>
+	// )
 	return (
-		
-		// <div className="wrapper">
-		// <Carditems img=" " title="Diet Coke" />
-		// <Carditems />
-		// <Carditems />
-		// </div>
-
-		<>
-			{foodItems.map(item => (
-				<ol key={item.id}>
-					<li>Name : {item.name}</li>
-					{item.description ? (
-						<li>Description : {item.description}</li>
-					) : (
-						<> </>
-					)}
-					<li>Price : {item.price}</li>
-					<br />
-				</ol>
-			))}
-		</>
+		<div>
+			<Grid container spacing={3}>
+				<Grid item xs={3}>
+					<Card sx={{ maxWidth: 345 }}>
+						<CardActionArea>
+							<CardMedia component='img' height='140' image='/static/images/cards/contemplative-reptile.jpg' alt='green iguana' />
+							<CardContent>
+								<Typography gutterBottom variant='h5' component='div'>
+									Diet Coke
+								</Typography>
+							</CardContent>
+						</CardActionArea>
+						<CardActions>
+							<Typography>Todo : increment and decrement quantity </Typography>
+							<Button variant='outlined' size='small' color='primary'>
+								Add to cart
+							</Button>
+						</CardActions>
+					</Card>
+				</Grid>
+			</Grid>
+		</div>
 	 )
 
 
