b72f4b297d6c2735a4e001eecb135ecbba737e6e
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-04-21 01:04:36 +0530

    Started About.js implementing using Material UI

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,12 +1,78 @@
-import React from 'react';
-import './About.css';
+import React, { useEffect } from 'react'
+import Axios from 'axios'
+import { AppBar, Box, Button, CardActions, Card, CircularProgress, Grid, IconButton, Toolbar, Typography, CardContent, CardMedia } from '@mui/material'
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt'
+
+const PeopleCard = props => {
+  return (
+    <>
+      <Card sx={{ maxWidth: 350 }}>
+        <CardMedia component='img' height='350vh' image={props.image} title={props.name} alt={props.name} />
+        <CardContent>
+          <Typography variant='h5' component='div'>
+            {props.name}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {props.details}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size='small'>TODO: Social Media</Button>
+        </CardActions>
+      </Card>
+    </>
+  )
+}
 
 const About = () => {
-	return (
-		<>
-			<h1>About Us</h1>
-		</>
-	);
-};
+  const [devs, setDevs] = React.useState(null)
 
-export default About;
+  const getDevsFromServer = async () => {
+    await Axios.get('http://localhost:5000/aboutus')
+      .then(({ data }) => {
+        setDevs(data)
+        console.log(data)
+      })
+      .catch(err => console.log(err))
+  }
+
+  useEffect(() => {
+    getDevsFromServer()
+  }, [])
+
+  return (
+    <>
+      {devs ? (
+        <>
+          <Box sx={{ flexGrow: 1 }}>
+            <AppBar position='static' sx={{ backgroundColor: 'orange', height: 65, maxHeight: 65 }}>
+              <Toolbar>
+                <IconButton size='large' edge='start' color='inherit' aria-label='menu' sx={{ mr: 2 }}>
+                  <PeopleAltIcon fontSize='large' />
+                </IconButton>
+                <Typography variant='h3' component='div' sx={{ flexGrow: 1 }}>
+                  About Us
+                </Typography>
+              </Toolbar>
+            </AppBar>
+            <Grid container sx={{ flexGrow: 1 }} justifyContent='center' alignItems='center' mt={'10%'}>
+              {devs.map(item => (
+                <Grid item xs={6} key={item.id} sx={{ padding: 10 }}>
+                  <PeopleCard image={item.image} name={item.name} details={item.details} phone={item.phone} email={item.email} />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        </>
+      ) : (
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+          <Grid item xs={3}>
+            <CircularProgress size={100} />
+          </Grid>
+        </Grid>
+      )}
+    </>
+  )
+}
+
+export default About
--- client/src/Pages/Menu/Menu.js
+++ client/src/Pages/Menu/Menu.js
@@ -4,36 +4,36 @@ import { CircularProgress } from '@mui/material'
 import CustomCard from './CustomCard'
 
 const Menu = () => {
-    const [foodItems, setFoodItems] = useState(null)
+  const [foodItems, setFoodItems] = useState(null)
 
-    const getFoodItems = async () => {
-        await Axios.get('http://localhost:5000/menu')
-            .then(res => {
-                setFoodItems(res.data)
-            })
-            .catch(err => console.log(err))
-    }
+  const getFoodItems = async () => {
+    await Axios.get('http://localhost:5000/menu')
+      .then(({ data }) => {
+        setFoodItems(data)
+      })
+      .catch(err => console.log(err))
+  }
 
-    useEffect(() => {
-        getFoodItems()
-    }, [])
+  useEffect(() => {
+    getFoodItems()
+  }, [])
 
-    return (
-        <div>
-            {foodItems ? (
-                foodItems.map(item => (
-                    <div id='cards' key={item.id}>
-                        <CustomCard id={item.id} image={item.image} name={item.name} />
-                        <br />
-                    </div>
-                ))
-            ) : (
-                <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', height: '100vh', width: '100vw' }}>
-                    <CircularProgress style={{ justifyContent: 'center', position: 'fixed', top: '50%' }} size={100} />
-                </div>
-            )}
+  return (
+    <div>
+      {foodItems ? (
+        foodItems.map(item => (
+          <div id='cards' key={item.id}>
+            <CustomCard id={item.id} image={item.image} name={item.name} />
+            <br />
+          </div>
+        ))
+      ) : (
+        <div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', height: '100vh', width: '100vw' }}>
+          <CircularProgress style={{ justifyContent: 'center', position: 'fixed', top: '50%' }} size={100} />
         </div>
-    )
+      )}
+    </div>
+  )
 }
 
 export default Menu
