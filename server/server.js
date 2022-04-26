6136207999d05113832cf41d0c21afb7f1a10276
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-04-26 23:06:44 +0530

    .

--- server/server.js
+++ server/server.js
@@ -7,68 +7,72 @@ app.use(cors())
 app.use(express.json())
 
 const db = mysql.createConnection({
-  user: 'admin',
-  password: 'Password',
-  database: 'fast_n_fresh',
-  host: 'fast-n-fresh.cfp2pum9jqtd.us-east-1.rds.amazonaws.com',
-  port: '3306',
+	user: 'admin',
+	password: 'Password',
+	database: 'fast_n_fresh',
+	host: 'fast-n-fresh.cfp2pum9jqtd.us-east-1.rds.amazonaws.com',
+	port: '3306',
 })
 
 app.post('/getUser', (req, res) => {
-  const email = req.body.email
-  const password = req.body.password
+	const email = req.body.email
+	const password = req.body.password
 
-  db.query(`SELECT * FROM users WHERE email = ? AND password = ?`, [email, password], (err, result) => {
-    if (err) {
-      res.send({ err: err })
-    }
+	db.query(`SELECT * FROM users WHERE email = ? AND password = ?`, [email, password], (err, result) => {
+		if (err) {
+			res.send({ err: err })
+		}
 
-    if (result.length > 0) {
-      res.send(result)
-    } else {
-      res.send({ message: 'Wrong password!' })
-    }
-  })
+		if (result.length > 0) {
+			res.send(result)
+		} else {
+			res.send({ message: 'Wrong password!' })
+		}
+	})
 })
 
 app.post('/signup', (req, res) => {
-  const name = req.body.name
-  const mobile = req.body.mobile
-  const email = req.body.email
-  const password = req.body.password
+	const name = req.body.name
+	const mobile = req.body.mobile
+	const email = req.body.email
+	const password = req.body.password
 
-  db.query('INSERT INTO users(email, password, name, mobile) VALUES (?, ?, ?, ?)', [email, password, name, mobile], (err, result) => {
-    if (err) {
-      res.send({ err: err })
-    }
-    if (result) {
-      res.send(result)
-    } else {
-      res.send({ message: 'User already exists!' })
-    }
-  })
+	db.query(
+		'INSERT INTO users(email, password, name, mobile) VALUES (?, ?, ?, ?)',
+		[email, password, name, mobile],
+		(err, result) => {
+			if (err) {
+				res.send({ err: err })
+			}
+			if (result) {
+				res.send(result)
+			} else {
+				res.send({ message: 'User already exists!' })
+			}
+		}
+	)
 })
 
 app.get('/menu', (req, res) => {
-  db.query(`SELECT * FROM menu`, (err, result) => {
-    if (err) {
-      res.send({ err: err })
-    } else {
-      res.send(result)
-    }
-  })
+	db.query(`SELECT * FROM menu`, (err, result) => {
+		if (err) {
+			res.send({ err: err })
+		} else {
+			res.send(result)
+		}
+	})
 })
 
 app.get('/aboutus', (req, res) => {
-  db.query(`SELECT * FROM about`, (err, result) => {
-    if (err) {
-      res.send({ err: err })
-    } else {
-      res.send(result)
-    }
-  })
+	db.query(`SELECT * FROM about`, (err, result) => {
+		if (err) {
+			res.send({ err: err })
+		} else {
+			res.send(result)
+		}
+	})
 })
 
 app.listen(5000, () => {
-  console.log('Listening at port 5000...')
+	console.log('Listening at port 5000...')
 })
