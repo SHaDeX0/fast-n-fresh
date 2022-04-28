56d0e647bb1635e720ecac0ab578b1c4a3361437
nprateekapai <nprateekapai@gmail.com>
2022-04-28 20:03:45 +0530

    .    .

--- client/src/Pages/About/About.js
+++ client/src/Pages/About/About.js
@@ -1,18 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
-import {
-	AppBar,
-	Box,
-	CardActions,
-	Card,
-	CircularProgress,
-	Grid,
-	IconButton,
-	Toolbar,
-	Typography,
-	CardContent,
-	CardMedia,
-} from '@mui/material'
+import { AppBar, Box, CardActions, Card, CircularProgress, Grid, IconButton, Toolbar, Typography, CardContent, CardMedia } from '@mui/material'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import EmailIcon from '@mui/icons-material/Email'
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt'
@@ -24,26 +12,13 @@ const PeopleCard = props => {
 
 	return (
 		<>
-			<Card
-				variant='elevation'
-				sx={{ maxWidth: 350, minWidth: 300, border: 1, borderColor: 'lightgrey', borderRadius: '2%' }}
-				onMouseOver={() => setElevation(10)}
-				onMouseOut={() => setElevation(3)}
-				elevation={elevation}
-			>
+			<Card variant='elevation' sx={{ maxWidth: 350, minWidth: 300, border: 1, borderColor: 'lightgrey', borderRadius: '2%' }} onMouseOver={() => setElevation(10)} onMouseOut={() => setElevation(3)} elevation={elevation}>
 				<CardContent>
 					<Typography variant='h5' component='div'>
 						{props.name}
 					</Typography>
 				</CardContent>
-				<CardMedia
-					component='img'
-					height='350vh'
-					image={props.image}
-					title={props.name}
-					alt={props.name}
-					sx={{ marginLeft: '2%', width: '96%', borderRadius: '2%' }}
-				/>
+				<CardMedia component='img' height='350vh' image={props.image} title={props.name} alt={props.name} sx={{ marginLeft: '2%', width: '96%', borderRadius: '2%' }} />
 				<CardActions>
 					<CardContent>
 						<Typography variant='body2' color='text.secondary'>
@@ -65,9 +40,7 @@ const PeopleCard = props => {
 						</IconButton>
 					</a>
 					<a
-						href={`https://api.whatsapp.com/send/?phone=${props.phone.replace(/[^\w\s]/gi, '').replace(/ /g, '')}&text=`
-							.concat(encodeURI(`Hello ${props.name}, I reached here from your Fast 'N Fresh website.`))
-							.concat('&app_absent=0')}
+						href={`https://api.whatsapp.com/send/?phone=${props.phone.replace(/[^\w\s]/gi, '').replace(/ /g, '')}&text=`.concat(encodeURI(`Hello ${props.name}, I reached here from your Fast 'N Fresh website.`)).concat('&app_absent=0')}
 						rel='noreferrer'
 						target='_blank'
 					>
@@ -75,13 +48,7 @@ const PeopleCard = props => {
 							<WhatsAppIcon />
 						</IconButton>
 					</a>
-					<a
-						href={`mailto:${props.email}?subject=Fast%20'N%20Fresh&body=`.concat(
-							encodeURI(`Hello ${props.name}, I reached here from your Fast 'N Fresh website.`)
-						)}
-						rel='noreferrer'
-						target='_blank'
-					>
+					<a href={`mailto:${props.email}?subject=Fast%20'N%20Fresh&body=`.concat(encodeURI(`Hello ${props.name}, I reached here from your Fast 'N Fresh website.`))} rel='noreferrer' target='_blank'>
 						<IconButton>
 							<EmailIcon />
 						</IconButton>
@@ -134,36 +101,17 @@ const About = () => {
 								</Toolbar>
 							</AppBar>
 						</Box>
-						<Grid
-							container
-							sx={{ flexGrow: 1, maxWidth: '95%', marginLeft: '2.5%' }}
-							justifyContent='center'
-							alignItems='center'
-							mt={'5%'}
-							mb={'2.5%'}
-							columnGap={5}
-							rowGap={5}
-						>
+						<Grid container sx={{ flexGrow: 1, maxWidth: '95%', marginLeft: '2.5%' }} justifyContent='center' alignItems='center' mt={'5%'} mb={'2.5%'} columnGap={5} rowGap={5}>
 							{devs.map(item => (
 								<Grid item container justifyContent='center' xs={3} key={item.id} sx={{ minWidth: 300 }}>
-									<PeopleCard
-										image={item.image}
-										name={item.name}
-										details={item.details}
-										phone={item.phone}
-										email={item.email}
-										instagram={item.instagram}
-										git={item.git}
-									/>
+									<PeopleCard image={item.image} name={item.name} details={item.details} phone={item.phone} email={item.email} instagram={item.instagram} git={item.git} />
 								</Grid>
 							))}
 						</Grid>
 					</Box>
 				</>
 			) : (
-				<div
-					style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', height: '100vh', width: '100vw' }}
-				>
+				<div style={{ alignItems: 'center', display: 'flex', justifyContent: 'center', height: '100vh', width: '100vw' }}>
 					<CircularProgress
 						style={{ color: 'tomato', justifyContent: 'center', position: 'fixed', top: '50%' }}
 						size={100}
